c468179615ae43b814694c9bed07d056cc2e739e
venkat <153338987+ChapaSriVenkat@users.noreply.github.com>
2025-09-01 10:23:23 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_connection():
     return mysql.connector.connect(
         host="98.86.9.11",
         user="root",
-        password="Srii@773",
+        password="Srii#773",
         database="datas"
     )
 
